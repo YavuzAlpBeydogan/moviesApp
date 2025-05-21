19e35244d3207e638002999d873294d9bba24423
Yavuz Alp Beydogan <alpixel@outlook.com>
2025-05-22 00:17:14 +0300

    güncelleme

--- movieApp/movies/models.py
+++ movieApp/movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Kategori Adı")
+
+class Movie(models.Model):
+    film_adi = models.CharField(max_length=200, verbose_name="Film Adı")
+    aciklama = models.TextField(verbose_name="Açıklama")
+    resim = models.CharField(max_length=200, verbose_name="Resim")
+    anasayfa = models.BooleanField(default=False, verbose_name="Anasayfada Göster")
--- movieApp/movies/views.py
+++ movieApp/movies/views.py
@@ -1,44 +1,23 @@
 from django.shortcuts import render
-
-kategori_liste = ["macera","romantik","dram","bilim kurgu"]
-film_liste=[
-    {
-        "film_adi":"film 1",
-        "aciklama":"film 1 açıklama",
-        "resim":"https://picsum.photos/200/300?random=1",
-        "anasayfa": True
-    },
-    {
-        "film_adi":"film 2",
-        "aciklama":"film 2 açıklama",
-        "resim":"https://picsum.photos/200/300?random=2",
-        "anasayfa": True
-    },
-    {
-        "film_adi":"film 3",
-        "aciklama":"film 3 açıklama",
-        "resim":"https://picsum.photos/200/300?random=3",
-        "anasayfa": False
-    },
-    {
-        "film_adi":"film 4",
-        "aciklama":"film 4 açıklama",
-        "resim":"https://picsum.photos/200/300?random=4",
-        "anasayfa": False
-    },
-    ]
+from .models import Category, Movie
 
 def home(request):
     data={
-        "kategoriler":kategori_liste,
-        "filmler":film_liste
+        "kategoriler":Category.objects.all(),
+        "filmler":Movie.objects.filter(anasayfa=True),
     }
     return render(request, "index.html", data)
 
 def movies(request):
     data={
-        "kategoriler":kategori_liste,
-        "filmler":film_liste
+        "kategoriler":Category.objects.all(),
+        "filmler":Movie.objects.all(),
     }
     return render(request,"movies.html", data)
 # Create your views here.
+
+def moviedetails(request,id):
+    data={
+        "movie":Movie.objects.get(id=id)
+    }
+    return render(request, "details.html", data)
